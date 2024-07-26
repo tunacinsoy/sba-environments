142a732d6961a7bb12aacb7a16f6957c9c8dbbae
Tuna Cinsoy <tunacinsoy@gmail.com>
2024-07-26 16:52:51 +0300

    fix: regional quota problem, setting ssd capacity

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -6,13 +6,16 @@ resource "google_service_account" "main" {
 resource "google_container_cluster" "main" {
   name               = "${var.cluster_name}-${var.branch}"
   location           = var.location
-  initial_node_count = 3
+  initial_node_count = 2
+
   node_config {
     service_account = google_service_account.main.email
+    disk_size_gb    = 10 # Setting disk size to 10 GB
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
+
   timeouts {
     create = "30m"
     update = "40m"
