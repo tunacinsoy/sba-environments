5985f7de3a159a035e55f82805014de4cad8fb29
tuna <tunacinsoy@gmail.com>
2024-08-22 21:24:08 +0200

    reinit

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,17 +1,17 @@
 # This file is responsible for the creation of gke cluster, and a service account.
 
-# resource "google_service_account" "main" {
-#   # Since there will be two clusters for 'prod' and 'dev' envs, we need to be able to
-#   # distinguish their service accounts.
-#   account_id   = "gke-${var.cluster_name}-${var.branch}-sa"
-#   display_name = "GKE Cluster ${var.cluster_name}-${var.branch} Service Account"
-# }
+resource "google_service_account" "main" {
+  # Since there will be two clusters for 'prod' and 'dev' envs, we need to be able to
+  # distinguish their service accounts.
+  account_id   = "gke-${var.cluster_name}-${var.branch}-sa"
+  display_name = "GKE Cluster ${var.cluster_name}-${var.branch} Service Account"
+}
 
-# After the creation of service account, the email attribute will be exposed automatically.
-# With locals definition, it will be more readable for users to see which attributes are created.
-# locals {
-#   service_account_email = google_service_account.main.email
-# }
+After the creation of service account, the email attribute will be exposed automatically.
+With locals definition, it will be more readable for users to see which attributes are created.
+locals {
+  service_account_email = google_service_account.main.email
+}
 
 resource "google_container_cluster" "main" {
   name               = "${var.cluster_name}-${var.branch}"
@@ -19,7 +19,7 @@ resource "google_container_cluster" "main" {
   initial_node_count = 3
 
   node_config {
-    # service_account = local.service_account_email # Retrieving the email of the service account from locals
+    service_account = local.service_account_email # Retrieving the email of the service account from locals
     disk_size_gb    = 10                          # Setting disk size to 10 GB because of the free account quota limits
     oauth_scopes = [
       # This scope is a Google Cloud OAuth scope that grants the client full access to all Google Cloud services.
