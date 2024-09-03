5fe133a8fa864e225acdf71a476e64b0a55e7b42
tuna <tunacinsoy@gmail.com>
2024-09-03 18:57:08 +0200

    retry

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,20 +1,20 @@
 # This hcl file is responsible for the configuration deployment that will be used by ArgoCD
 
-# # ApplicationSet resource for the applications that argoCD will manage
-# data "kubectl_file_documents" "apps" {
-#   content = file("../manifests/argocd/apps.yaml")
-# }
+# ApplicationSet resource for the applications that argoCD will manage
+data "kubectl_file_documents" "apps" {
+  content = file("../manifests/argocd/apps.yaml")
+}
 
-# resource "kubectl_manifest" "apps" {
-#   # Needs to depend on argocd deployment, since we'll configure it after deployment finishes
-#   depends_on = [kubectl_manifest.argocd]
-#   # for_each iterates over each manifest in the namespace file
-#   for_each = data.kubectl_file_documents.apps.manifests
-#   # Applies the content of each manifest to the Kubernetes cluster
-#   yaml_body = each.value
-#   # Forces the namespace to be set to argocd, ensuring that all resources are created in the correct namespace
-#   override_namespace = "argocd"
-# }
+resource "kubectl_manifest" "apps" {
+  # Needs to depend on argocd deployment, since we'll configure it after deployment finishes
+  depends_on = [kubectl_manifest.argocd]
+  # for_each iterates over each manifest in the namespace file
+  for_each = data.kubectl_file_documents.apps.manifests
+  # Applies the content of each manifest to the Kubernetes cluster
+  yaml_body = each.value
+  # Forces the namespace to be set to argocd, ensuring that all resources are created in the correct namespace
+  override_namespace = "argocd"
+}
 
 # I am done with externalSecrets, a lot of problems
 # Managing Secrets using ExternalSecrets Operator
